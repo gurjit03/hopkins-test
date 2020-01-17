c38db9bf6d61e71101355c9e57b869651a4c6ec7
gurjit03 <gurjitmehta@gmail.com>
2020-01-18 03:16:13 +0530

    cleaned up the console.logs

--- src/utils/get-date-from-current-day.js
+++ src/utils/get-date-from-current-day.js
@@ -3,13 +3,6 @@ import { DATE_FORMAT } from "../config";
 
 // @Function returns the date object given the value of current day ( where current day == 1 means it's today)
 export const getDateFromCurrentDay = currentDay => {
-  const daysToAdd = ``;
-  console.log(
-    daysToAdd,
-    moment()
-      .add(currentDay - 1, "days")
-      .format(DATE_FORMAT)
-  );
   return moment()
     .add(currentDay - 1, "days")
     .format(DATE_FORMAT);
